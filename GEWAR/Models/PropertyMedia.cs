1c493a8e6184bdab49c3ae136c5c062bddd58455
samar <samarmustafa@gmail.com>
2025-09-28 14:42:44 +0300

    fixing

--- GEWAR/Models/PropertyMedia.cs
+++ GEWAR/Models/PropertyMedia.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace GEWAR.Models
 {
-    internal class PropertyMedia
+    public class PropertyMedia
     {
+        public enum MediaTypeEnum
+        {
+            Image,
+            Video,
+            FloorPlan,
+            _360Tour 
+        }
+
+        public int MediaID { get; set; }
+        public int PropertyID { get; set; }
+        public string MediaURL { get; set; }
+        public string MediaType { get; set; }
+        public DateTime UploadedDate { get; set; }
+
+        public static bool IsValidMediaType(string mediaType)
+        {
+            if (mediaType == "360Tour")
+                return true;
+
+            return Enum.TryParse<MediaTypeEnum>(mediaType, out var result) &&
+                   Enum.IsDefined(typeof(MediaTypeEnum), result);
+        }
     }
 }
---
+++ GEWAR/Models/RenovationCost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEWAR.Models
+{
+    internal class RenovationCost
+    {
+       
+            public int CostID { get; set; }
+            public int ProjectID { get; set; }
+            public string CostType { get; set; }
+            public decimal? EstimatedValue { get; set; }
+        }
+    }
+
